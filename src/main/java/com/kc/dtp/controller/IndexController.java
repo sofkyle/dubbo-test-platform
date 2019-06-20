731f476352e9dced9ab9b3ed3762af332e66bf55
zhouyongfa <fightzyf1992@hotmail.com>
2019-06-20 22:49:13 +0800

    temp save

--- src/main/java/com/kc/dtp/controller/IndexController.java
+++ src/main/java/com/kc/dtp/controller/IndexController.java
@@ -1,19 +1,25 @@
 package com.kc.dtp.controller;
 
-import org.springframework.stereotype.Controller;
+import com.kc.dtp.handler.ApiHandler;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * @author: Kyle
  */
 @RestController
+@RequestMapping("/view")
 public class IndexController {
 
-    @GetMapping("/index")
+    @Resource
+    private ApiHandler apiHandler;
+
+    @RequestMapping("/index")
     public String listPage(final Model model) {
-        return "index";
+        // final Mono<Integer> api = apiHandler.addApi("url");
+        return "view/index";
     }
 }
--- src/main/java/com/kc/dtp/handler/ApiHandler.java
+++ src/main/java/com/kc/dtp/handler/ApiHandler.java
@@ -1,14 +1,19 @@
 package com.kc.dtp.handler;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
 /**
  * @author: Kyle
  */
-@Component
+@Component("apiHandler")
 public class ApiHandler {
-    public Mono<Integer> addApi(String url) {
-        return Mono.just(1);
+    public Mono<ServerResponse> addApi(ServerRequest request) {
+        return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN)
+                .body(BodyInserters.fromObject("Hello, City!"));
     }
 }
